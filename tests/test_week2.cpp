f14fb7ff2fa734c0fec013f4a0108994678f950e
Mariza Sitcheu <mariza.yamdjeu.sitcheu@uni-jena.de>
2026-04-22 21:02:30 +0200

    feat: benchmark permutation

--- tests/test_week2.cpp
+++ tests/test_week2.cpp
@@ -5,18 +5,6 @@
 #include "week2/fmadd.hpp"
 
 
-// Reference code to verify your Assembly
-void perm_cpp_abc_cba(int64_t size_c, float const * abc, float * cba) {
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            for (int k = 0; k < size_c; ++k) {
-                int64_t in_idx = i * (4 * size_c) + j * size_c + k;
-                int64_t out_idx = k * 32 + j * 8 + i;
-                cba[out_idx] = abc[in_idx];
-            }
-        }
-    }
-}
 
 TEST_CASE("FMADD Benchmark", "[fmadd]") {
     SECTION("Benchmark FMADD") {
